d46f4320cf47d7e42686c12b2ed82ed6ea612565
fidlerdev <fidlerdev@yandex.ru>
2024-04-04 03:55:36 +0300

    Исправлена коллизия

--- ball.py
+++ ball.py
@@ -19,6 +19,7 @@ class Ball(GameSprite):
         self.game = game
 
         # Рандомно выбираем сторону угол в одной из 4 четвертей
+        # (магические числа!)
         seed(time())
         self.angle = choice(
             [
@@ -32,34 +33,55 @@ class Ball(GameSprite):
         self.spawn_time = pg.time.get_ticks()
         self.player_hit_time = pg.time.get_ticks()
 
+
+    @property
+    def rangle(self):
+        """При столкновении с игроком будем добавлять небольшой рандомный угол
+            для интереса"""
+        return choice([angle / 100 for angle in range(-30, 30)])
+
     def update(self,  *args, **kwargs) -> None:
+        # Ссылки на объекты игроков
+        playerL: Player = self.game.playerL
+        playerR: Player = self.game.playerR
+
+        # Сначала движение по оси X
         self.rect.x += cos(self.angle) * self.speed * self.game.delta_time
+
+        if self.rect.colliderect(playerL.rect):
+            self.speed = -self.speed
+            self.rect.left = playerL.rect.right
+            self.angle = tau - self.angle + self.rangle
+
+        if self.rect.colliderect(playerR.rect):
+            self.speed = -self.speed
+            self.rect.right = playerR.rect.left
+            self.angle = tau - self.angle + self.rangle
+
         self.rect.y += sin(self.angle) * self.speed * self.game.delta_time
+
 
         # Чем дольше мяч живет, тем быстрее он становится
         now: int = pg.time.get_ticks()
         multiplier: float = (now - self.spawn_time) / 10000000
+
         # Если скорость не больше максимальной
         if abs(self.speed) <= BALL_MAX_SPEED:
             self.speed *= (1 + multiplier)
 
         # Коллизия с верхней и нижней стеной
-        if (self.rect.y >= W_HEIGHT - self.rect.width or
-            self.rect.y <= 0):
-            self.last_angle_change = now
+        top_collision = self.rect.top < 0
+        bottom_collision = self.rect.bottom > W_HEIGHT
 
+        if top_collision or bottom_collision:
             self.angle = pi - self.angle
             self.speed = -self.speed
 
-        # Коллизия с игроком
-        playerCol: list[Player] = pg.sprite.spritecollide(self, self.game.players, False)
+            if bottom_collision:
+                self.rect.bottom = W_HEIGHT
 
-        if playerCol and (now - self.player_hit_time) > PLAYER_HIT_DELAY:
-            self.angle = tau - self.angle
-            self.speed = -self.speed
-            self.game.who_touched_last = playerCol[0]
-            self.player_hit_time = now
-            choice(self.game.audio.hit_sounds).play()
+            if top_collision:
+                self.rect.top = 0
 
         # Мяч улетает за экран (пока оба случая)
         if self.rect.x > W_WIDTH or self.rect.x + self.rect.width < 0:
